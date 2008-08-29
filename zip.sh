71875141566f3bac50f328f29d25bdb31c07691a
ulrich.hecht <ulrich.hecht@7eb1d588-9955-0410-871b-cb72e4dc1803>
2008-08-29 21:49:30 +0000

    handle file-not-found on resume gracefully (fall back to file dialog) don't zip config file

--- zip.sh
+++ zip.sh
@@ -2,4 +2,4 @@
 cp -p lecturer_tomtom Lecturer/lecturer
 arm-linux-strip Lecturer/lecturer
 rm -f lecturer.zip
-7z a -tzip '-xr!.svn' '-x!*/text/*.txt' lecturer.zip SDKRegistry Lecturer
+7z a -tzip '-xr!.svn' '-x!*/text/*.txt' '-x!*/conf' lecturer.zip SDKRegistry Lecturer
