5d0833ab7006f7100080a5a33ddd955faebc1923
ulrich.hecht <ulrich.hecht@7eb1d588-9955-0410-871b-cb72e4dc1803>
2008-09-02 23:04:00 +0000

    make progress bar touch area smaller and disable when bar not drawn zip.sh: add revision number to zip file name

--- zip.sh
+++ zip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+rev=r`svn info|grep ^Revision|sed "s,^Revision: ,,"`
 cp -p lecturer_tomtom Lecturer/lecturer
 arm-linux-strip Lecturer/lecturer
-rm -f lecturer.zip
-7z a -tzip '-xr!.svn' '-x!*/text/*' '-x!*/conf' lecturer.zip SDKRegistry Lecturer
+rm -f lecturer-$rev.zip
+7z a -tzip '-xr!.svn' '-x!*/text/*' '-x!*/conf' lecturer-$rev.zip SDKRegistry Lecturer
